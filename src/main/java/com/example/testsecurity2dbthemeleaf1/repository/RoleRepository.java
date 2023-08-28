f95a68a72c20b0fa4a3fc86c4cd18baa36d3a3a4
daniil <podryadov89@mail.ru>
2023-08-29 00:22:35 +0500

    lr7

---
+++ src/main/java/com/example/testsecurity2dbthemeleaf1/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.testsecurity2dbthemeleaf1.repository;
+
+import com.example.testsecurity2dbthemeleaf1.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
